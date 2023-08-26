c9335959e16cc7c010ecf82a4d8178cb950e89d3
chiliat <1299378216@qq.com>
2023-08-26 21:17:58 +0800

    排序

--- test/Sort/Sort.c
+++ test/Sort/Sort.c
@@ -9,6 +9,13 @@ void printArray(int* array, int n)
 	printf("\n");
 }
 
+void swap(int* array, int num1, int num2)
+{
+	int tem = array[num1];
+	array[num1] = array[num2];
+	array[num2] = tem;
+}
+
 void insertSort(int* array, int n)
 {
 	for (int i = 0; i < n - 1; i++)
@@ -31,7 +38,56 @@ void insertSort(int* array, int n)
 	}
 }
 
-void ShellSort(int* array, int n)
+void SelectSort(int* array, int n)
+{
+	int begin = 0;
+	int end = n - 1;
+	while (begin < end)
+	{
+		int max = end;
+		int min = begin;
+		for (int i = begin + 1; i <= end; i++)
+		{
+			if (array[i] < array[min])
+			{
+				min = i;
+			}
+			if (array[max] < array[i])
+			{
+				max = i;
+			}
+		}
+		if (max == begin)
+		{
+			max = min;
+		}
+		swap(array, begin, min);
+		swap(array, end, max);
+		begin++;
+		end++;
+	}
+}
+
+void bubbleSort(int* array, int n)
+{
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		bool flag = true;
+		for (int j = 0; j < n - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array, j, j + 1);
+				flag = false;
+			}
+		}
+		if (flag)
+			break;
+	}
+}
+
+void shellSort(int* array, int n)
 {
 	int gap = n;
 	while (gap > 1)
@@ -57,4 +113,72 @@ void ShellSort(int* array, int n)
 			array[end + gap] = temp;
 		}
 	}
+}
+
+void adjustDwon(int* array, int size, int parent)
+{
+	int child = parent * 2 + 1;
+	if (child + 1 < size && array[child + 1] < array[child])
+	{
+		child++;
+	}
+	while (array[child] < array[parent])
+	{
+		if (child + 1 < size && array[child + 1] < array[child])
+		{
+			child++;
+		}
+		if (size <= child)
+		{
+			break;
+		}
+		swap(array, child, parent);
+		parent = child;
+		child = parent * 2 + 1;
+
+	}
+}
+
+void heapSort(int* array, int size)
+{
+	//ÉıĞò Ğ¡¶¥¶Ñ 
+	//1.½¨¶Ñ
+	for (int i = size - 1; i >= 0; i--)
+	{
+		adjustDwon(array, size, i);
+	}
+	//2.µ÷Õû£¬ÅÅĞò
+	for (int j = 1; j < size; j++)
+	{
+		swap(array, 0, size - j);
+		adjustDwon(array, size - j, 0);
+	}
+}
+
+void quickSort(int* array, int begin, int end)
+{
+	if (begin >= end)
+	{
+		return;
+	}
+
+	int left = begin;
+	int right = end;
+	int key = begin;
+	while (left < right)
+	{
+		while (left < right && array[right] >= array[key])
+		{
+			right--;
+		}
+		while (left < right && array[left] <= array[key])
+		{
+			left++;
+		}
+		swap(array, key, left);
+	}
+	key = left;
+
+	quickSort(array, begin, key - 1);
+	quickSort(array, key + 1, end);
 }
--- test/Sort/Sort.h
+++ test/Sort/Sort.h
@@ -1,8 +1,17 @@
 #pragma once
 #include<stdio.h>
+#include<stdbool.h>
 
 void printArray(int* array, int n);
 //²åÈëÅÅĞò
 void insertSort(int* array, int n);
+//Ñ¡ÔñÅÅĞò
+void SelectSort(int* array, int n);
+// Ã°ÅİÅÅĞò
+void bubbleSort(int* array, int n);
 //Ï£¶ûÅÅĞò
-void ShellSort(int* array, int n);
+void shellSort(int* array, int n);
+//¶ÑÅÅĞò
+void heapSort(int* array, int size);
+//¿ìËÙÅÅĞò
+void quickSort(int* array, int begin, int end);
