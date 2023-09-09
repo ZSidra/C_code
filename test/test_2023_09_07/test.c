20528549a91d865f2b638dfb4f97d8dfcb9791b4
chiliat <1299378216@qq.com>
2023-09-09 12:53:00 +0800

    练习代码提交

---
+++ test/test_2023_09_07/test.c
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+/*********Begin*********/
+typedef struct Fool
+{
+    char name[20];
+    int num;
+}Fool;
+
+/*********End**********/
+int main(void)
+{
+    /*********Begin*********/
+    int n = 0;
+    Fool f1, f2, f3;
+    strcpy(f1.name, "Li");
+    strcpy(f2.name, "Sun");
+    strcpy(f3.name, "Zhang");
+    f1.num = f2.num = f3.num = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        char temp[20];
+        scanf("%s", temp);
+        if (strcmp(temp, f1.name))
+        {
+            f1.num++;
+        }
+        else if (strcmp(temp, f2.name))
+        {
+            f2.num++;
+        }
+        else if (strcmp(temp, f3.name))
+        {
+            f3.num++;
+        }
+    }
+
+    printf("%s:%d\n", f1.name, f1.num);
+    printf("%s:%d\n", f3.name, f3.num);
+    printf("%s:%d", f2.name, f2.num);
+
+    /*********End**********/
+    return 0;
+}
